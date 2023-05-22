1b34a92bfe2cf8cef15d833ceaab279fea787b42
Caio Felipe <93288204+caioskywalker@users.noreply.github.com>
2023-05-22 18:05:16 -0300

    Add files via upload

--- Projeto1Ebac/src/br/com/cfarias/dao/ClienteSetDAO.java
+++ Projeto1Ebac/src/br/com/cfarias/dao/ClienteSetDAO.java
@@ -1,4 +1,51 @@
 package br.com.cfarias.dao;
 
-public class ClienteSetDAO {
+import java.util.Collection;
+import java.util.HashSet;
+import br.com.cfarias.domain.Cliente;
+
+public class ClienteSetDAO implements IClienteDAO {
+	
+	private HashSet<Cliente>cadastrarCliente;
+	Cliente clienteCadastrado = new Cliente(null, null, null, null, null, null, null, null);
+	
+	
+	public ClienteSetDAO() {
+		this.cadastrarCliente = new HashSet<>();
+		
+	}
+
+	
+	public Boolean cadastrar(Cliente cliente) {
+		return null;
+	}
+
+
+	public void excluir(Long cpf) {
+		
+
+		
+	}
+
+
+	public void alterar(Cliente cliente) {
+
+		
+	}
+
+	
+	public Cliente consultar(Long cpf) {
+		
+		return null;
+	}
+
+	
+	public Collection<Cliente> buscarTodos() {
+		
+		return null;
+	}
+	
+	
+	
+	
 }
